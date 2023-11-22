6c374bb2e05904bfb474398100eb0edd5813f6e5
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2023-11-22 20:34:47 +0000

    Now made with forEach

--- src/app.js
+++ src/app.js
@@ -11,16 +11,16 @@ window.onload = function() {
   let noun = ["apple", "chair", "face", "road"];
   let domain = [".com", ".io", ".net", ".org"];
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let x = 0; x < adj.length; x++) {
-      for (let j = 0; j < noun.length; j++) {
-        for (let y = 0; y < domain.length; y++) {
+  pronoun.forEach(i => {
+    adj.forEach(x => {
+      noun.forEach(j => {
+        domain.forEach(y => {
           let list = document.createElement("li");
           list.className = "text-primary";
-          list.innerText = `${pronoun[i]}${adj[x]}${noun[j]}${domain[y]}`;
+          list.innerText = `${i}${x}${j}${y}`;
           document.querySelector("#domains").appendChild(list);
-        }
-      }
-    }
-  }
+        });
+      });
+    });
+  });
 };
